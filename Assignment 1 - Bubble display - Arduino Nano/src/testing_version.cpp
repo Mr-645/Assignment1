0930d5cb38de9f0e8fc9681ed0e081278b643660
Prab Singh <prab-singh@live.com>
2019-05-15 12:11:41 +1200

    Added comments to code Added datasheet image Updated README file

--- Assignment 1 - Bubble display - Arduino Nano/src/testing_version.cpp
+++ Assignment 1 - Bubble display - Arduino Nano/src/testing_version.cpp
@@ -3,22 +3,23 @@
 
 #define interruptPinStartStop 2 //The Start/Stop button pin
 #define interruptPinReset 3 //The Reset button pin
-#define ledPin 13 //LED builtin pin for showing when the debounce function is ignoring button presses
+#define ledPin 13 //LED builtin pin
 
 uint32_t debounceDelay = 50;  //Debounce time - Switch ignore time
 uint32_t lastDebounceTime = 0; //This will store the last time the LED was updated
-bool debounceDelayDone = false; //Whether or not the debounce delay has occurred
-bool allowTiming = false; //Whether or not the stopwatch should run
+bool debounceDelayDone = false; //Whether or not the debounce delay has occured
+bool allowTiming = false;
 
-//An instance of the object.
+//Create an instance of the object.
 SevSeg myDisplay;
 
-//Global variables
-uint32_t startTimePoint = 0; //Point at which the stopwatch starts from a reset or the micro turning on
-uint32_t timeWhilstStopped = 0; //Tracks end time points for the latest stopped time period
-uint32_t TimeOfStopWatch = 0; //Tracks the end time point for the latest run time period
+//Create global variables
+uint32_t startTimePoint = 0;
+uint32_t timeWhilstStopped = 0;
+uint32_t TimeOfStopWatch = 0;
+uint8_t stageVal = 0;
 
-//Function to handle start/stop functionality
+//Function to give clearance for serial output
 void startStopFunc(){
   if (debounceDelayDone == true) {
     //If debounceDelayDone is true, do the following 
@@ -26,36 +27,42 @@ void startStopFunc(){
     //Toggle between the modes
     if (allowTiming == true){
       allowTiming = false;
+      // stageVal = 1;
     }
+    //else if (allowTiming == false){
     else{
       allowTiming = true;
+      // stageVal = 2;
     }
     lastDebounceTime = millis(); //Reset last debounce time
   }
 }
 
-//Function to handle reset functionality
+//Function to give clearance for serial output
 void resetFunc(){
   if (debounceDelayDone == true) {
     //If debounceDelayDone is true, do the following
-	//Reset all the variables
     TimeOfStopWatch = 0;
     startTimePoint = 0;
     timeWhilstStopped = 0;
     allowTiming = false;
     lastDebounceTime = millis(); //Reset last debounce time
+    // stageVal = 0;
   }
 }
 
 void setup(){
-  int displayType = COMMON_CATHODE; //The .cpp file underneath (in the library) had to be modified
-       //Pins that are connected to the Mosfet gate pins (to sink the cathodes to GND)
+  Serial.begin(9600); //Initialises serial comms at a baud rate of 9600
+
+  int displayType = COMMON_CATHODE; //Your display is either common cathode or common anode
+    //This pinout is for a bubble dispaly
+       //Declare what pins are connected to the GND pins (cathodes)
        int digit1 = 10; 
        int digit2 = 12; 
        int digit3 = 11; 
        int digit4 = A0;
        
-       //Pins that are connected to the segments (anodes)
+       //Declare what pins are connected to the segments (anodes)
        int segA = A1; 
        int segB = A2; 
        int segC = 4; 
@@ -65,7 +72,7 @@ void setup(){
        int segG = 8; 
        int segDP= 9; 
    
-  int numberOfDigits = 4; //4 digit display
+  int numberOfDigits = 4; //Do you have a 1, 2 or 4 digit display?
 
   myDisplay.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, segA, segB, segC, segD, segE, segF, segG, segDP);
   
@@ -75,19 +82,18 @@ void setup(){
   pinMode(interruptPinStartStop, INPUT_PULLUP); //Assigns a pull up resistor to the push button
   pinMode(interruptPinReset, INPUT_PULLUP); //Assigns a pull up resistor to the push button
 
-  //Run the interruptPinStartStop function when the pin goes low
+  //Run the dispValFunc function when the pin is high
   attachInterrupt(digitalPinToInterrupt(interruptPinStartStop), startStopFunc, FALLING);
 
-  //Run the interruptPinReset function when the pin goes low
+  //Run the dispValFunc function when the pin is high
   attachInterrupt(digitalPinToInterrupt(interruptPinReset), resetFunc, FALLING);
 }
 
 void loop(){
-  //Display the floating point number that the "TimeOfStopWatch" variable is associated with
+  //Example ways of displaying a decimal number
   char tempString[10]; //Used for sprintf
   sprintf(tempString, "%4d", TimeOfStopWatch); //Convert TimeOfStopWatch into a string that is right adjusted
 
-  //Handle the placement of the decimal point
   if (TimeOfStopWatch >= 1000 && TimeOfStopWatch < 10000){
     myDisplay.DisplayString(tempString, 1);
   }
@@ -102,35 +108,49 @@ void loop(){
   //Pressed start
   if(allowTiming == true){
     if(TimeOfStopWatch == 0){
-      startTimePoint = millis(); //Keeps updating the start time point until start is pressed
-      TimeOfStopWatch++; //This just gets the variable going for the loop to handle next time
+      startTimePoint = millis();
+      TimeOfStopWatch++;
+      // stageVal = 1;
     }
     else if(timeWhilstStopped == 0){
-      TimeOfStopWatch = millis() - timeWhilstStopped - startTimePoint; //Main sums for stop watch running before the first stop
+      TimeOfStopWatch = millis() - timeWhilstStopped - startTimePoint;
+      // stageVal = 2;
     }
     else{
-      TimeOfStopWatch = millis() - timeWhilstStopped; //Main sums for all following stopwatch stop/starts
+      TimeOfStopWatch = millis() - timeWhilstStopped;
+      // stageVal = 3;
     }
   }
   //Pressed stop
   else if(allowTiming == false) {
     if(TimeOfStopWatch != 0){
-      timeWhilstStopped = millis() - TimeOfStopWatch; //Keeps track of how long we were stopped for
+      timeWhilstStopped = millis() - TimeOfStopWatch;
+      // stageVal = 4;
     }
     else{
-      timeWhilstStopped = 0; //Housekeeping for when the stopwatch is reset
+      timeWhilstStopped = 0;
+      // stageVal = 5;
     }
   }
   // Main timing stuff stops here
-
+  // /*
+  Serial.print(TimeOfStopWatch);
+  Serial.print(",");
+  Serial.print(timeWhilstStopped);
+  Serial.print(",");
+  Serial.print(startTimePoint);
+  Serial.print(",");
+  Serial.print(stageVal);
+  Serial.println(",");
+  // */
 
   //Take care of switch debouncing
   if (millis() - lastDebounceTime >= debounceDelay){
     debounceDelayDone = true;
-    digitalWrite(ledPin, LOW); //Turns the builtin LED off if the debounce timer has finished (not ignoring inputs)
+    digitalWrite(ledPin, LOW);
   }
   else {
     debounceDelayDone = false;
-    digitalWrite(ledPin, HIGH); //Turns the LED on if the timer is still running (ignoring inputs)
+    digitalWrite(ledPin, HIGH);
   }
 }
--- README.md
+++ README.md
@@ -26,7 +26,7 @@ sink and source a limited current
 
 ### Description of design process steps
 
-Bubble display is of type `Common Cathode` ... this means ...
+Bubble display is of type `Common Cathode` ... this means ... had to modify .cpp file of library to make it compliant with my system
 
 These is the pin configuration ...
 
@@ -36,38 +36,31 @@ This is how the timing works ... you only need these variables ... <span style="
   //Pressed start
   if(allowTiming == true){
     if(TimeOfStopWatch == 0){
-      startTimePoint = millis();
-      TimeOfStopWatch++;
-      // stageVal = 1;
+      startTimePoint = millis(); //Keeps updating the start time point until start is pressed
+      TimeOfStopWatch++; //This just gets the variable going for the loop to handle next time
     }
     else if(timeWhilstStopped == 0){
-      TimeOfStopWatch = millis() - timeWhilstStopped - startTimePoint;
-      // stageVal = 2;
+      TimeOfStopWatch = millis() - timeWhilstStopped - startTimePoint; //Main sums for stop watch running before the first stop
     }
     else{
-      TimeOfStopWatch = millis() - timeWhilstStopped;
-      // stageVal = 3;
+      TimeOfStopWatch = millis() - timeWhilstStopped; //Main sums for all following stopwatch stop/starts
     }
   }
   //Pressed stop
   else if(allowTiming == false) {
     if(TimeOfStopWatch != 0){
-      timeWhilstStopped = millis() - TimeOfStopWatch;
-      // stageVal = 4;
+      timeWhilstStopped = millis() - TimeOfStopWatch; //Keeps track of how long we were stopped for
     }
     else{
-      timeWhilstStopped = 0;
-      // stageVal = 5;
+      timeWhilstStopped = 0; //Housekeeping for when the stopwatch is reset
     }
   }
-  // Main timing stuff stops here
 ```
 
 ---
 
 ### Component choice and Schematic
 
-	
 Microcontroller and bubble display can only handle so much current ...
 
 Microcontroller can only source/sink this much ... bubble display can only handle 5 mA per segment ... adequate brightness achieved at x potentiometer resistance. <span style="background-color: #FFFF00">`Datasheet is in appendix`
@@ -77,14 +70,20 @@ For circit safety ... potentiometer ... resistors in series with each anode (330
 For reliable performance ... pulldown resistors on mosfet gates (5.6 kΩ)
 
 <img src="/Circuit.jpg" alt="Schematic" width="200">
-<img src="/3D_view.jpg" alt="Schematic" width="200">
+<img src="/3D_view.jpg" alt="3D view" width="200">
 
 ---
 
 ### Limitations of project and potential improvements
-Blah
+It worked really well most of the time.
+Sometimes there are unidentifiable glitches when a button press happens
 
 ---
 
 ### Appendix
-Blah
+#### Datasheet
+
+<img src="/datasheet.png" alt="Datasheet" width="500">
+
+#### Source code of non-testing version of programme
+The source code (main.cpp) is available [here](https://github.com/Mr-645/Assignment1/blob/master/Assignment%201%20-%20Bubble%20display%20-%20Arduino%20Nano/src/main.cpp)
